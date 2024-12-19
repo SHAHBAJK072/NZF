c5299e9eb3e9c70a4a0bc51fdc6645cc3359676d
SHAHBAJK072 <shabajk072@gmail.com>
2024-12-19 17:59:25 +0530

    NZF

---
+++ js/animated.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".slider", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+      },
+    pagination: {
+      el: ".dots",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".next",
+      prevEl: ".prev",
+    },
+    breakpoints:{
+      1200: {
+        slidesPerView:3,
+        spaceBetween:24,
+      },
+      // 992: {
+      //   slidesPerView:2,
+      //   spaceBetween:24,
+      // },
+      768: {
+        slidesPerView:2,
+        spaceBetween:24,
+      },  
+      0: {
+        slidesPerView:1,
+      },
+  
+    }
+    
+  });
+
+  jQuery(document).on('mouseover', '.hero-img .img-box', function () {
+    jQuery('.hero-img .img-box.active').removeClass('active');
+    jQuery(this).addClass('active');});
+    
